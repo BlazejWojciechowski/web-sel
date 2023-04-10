96d5c8b52ac41ba75ca4961e036ce683b3d11bf8
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-10 10:09:19 +0200

    initial commit

---
+++ src/test/java/blaze/springframework/WebSelApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
