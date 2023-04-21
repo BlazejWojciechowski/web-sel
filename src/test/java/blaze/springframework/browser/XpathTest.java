8da2462371ed3a81aa0bcdd0f1a0a984e6662db0
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-21 10:42:33 +0200

    added some extra Xpath methods

--- src/test/java/blaze/springframework/browser/XpathTest.java
+++ src/test/java/blaze/springframework/browser/XpathTest.java
@@ -1,6 +1,5 @@
 package blaze.springframework.browser;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
