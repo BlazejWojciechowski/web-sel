1892424e98bebf54fc59e9e377d44d55c8979441
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-24 19:25:37 +0200

    added tests with testng annotations

---
+++ src/test/java/blaze/springframework/testng/FirstTest.java
@@ -0,0 +1,49 @@
+package blaze.springframework.testng;
+
+import blaze.springframework.actions.BasicActionsTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstTest extends BaseTest {
+    @Test
+    public void firstTest() {
+
+        BasicActionsTest basicActionsTest = new BasicActionsTest();
+        WebDriver driver = basicActionsTest.getDriver("firefox");
+        driver.manage().window().maximize();
+        driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+
+        driver.findElement(By.id("clickOnMe")).click();
+
+        Duration time = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver,time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+        driver.findElement(By.cssSelector("p"));
+
+        String paraText = driver.findElement(By.cssSelector("p")).getText();
+        Assert.assertEquals(paraText, "Dopiero się pojawiłem!");
+    }
+    @Test
+    public void secondTest() {
+
+        BasicActionsTest basicActionsTest = new BasicActionsTest();
+        WebDriver driver = basicActionsTest.getDriver("firefox");
+        driver.manage().window().maximize();
+        driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+
+        driver.findElement(By.id("clickOnMe")).click();
+
+        Duration time = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver,time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+
+
+        driver.findElement(By.cssSelector("p"));
+    }
+}
--- src/test/java/blaze/springframework/wait/WaitTest.java
+++ src/test/java/blaze/springframework/wait/WaitTest.java
@@ -5,15 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Function;
 
 public class WaitTest {
     WebDriver driver;
