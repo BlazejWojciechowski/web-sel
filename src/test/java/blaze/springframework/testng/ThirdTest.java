15dd30c3d55f64cb37418974cade231acaebcc3e
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-25 10:18:54 +0200

    added priority in ThirdTest class

---
+++ src/test/java/blaze/springframework/testng/ThirdTest.java
@@ -0,0 +1,18 @@
+package blaze.springframework.testng;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest{
+    @Test(priority = 0)
+    public void firstTest() {
+        System.out.println("I am first test.");
+    }
+    @Test(priority = 2)
+    public void secondTest() {
+        System.out.println("I am second test.");
+    }
+    @Test(priority = 1)
+    public void thirdTest() {
+        System.out.println("I am third test.");
+    }
+}
